a3e53dcf96e997a7c7111197902a909e67733503
liyanzhu <296652033@qq.com>
2020-07-20 10:31:46 +0800

    sencond

--- js/top.js
+++ js/top.js
@@ -12,44 +12,55 @@ document.addEventListener('scroll', function () {
   else {
     topmore.style.transform = 'scale(0)';
   }
-  
 
+  //改变侧边栏的颜色
+  for (let i = 0; i < tops.length; i++) {
+    if (tops[i].top < window.pageYOffset && tops[i].top + tops[i].height -100 > window.pageYOffset  ) {
+      for (let j = 0; j < tops.length; j++) {
+        tops[j].style.backgroundColor = ''
+      }
+      tops[i].style.backgroundColor = tops[i].getAttribute('color');
+    }
+  }
 })
-var stop = document.documentElement.scrollTop;
-var tops = document.querySelectorAll('.topmore a');
-tops[1].onclick = function () {
-  for (let i = 0; i < tops.length; i++) {
-    tops[i].style.backgroundColor = ''
+//对侧边栏处理
+var conList = [
+    document.querySelector('.main>div:nth-child(3)'),
+    document.querySelector('.main>div:nth-child(5)'),
+    document.querySelector('.main>div:nth-child(6)'),
+    document.querySelector('.main>div:nth-child(8)'),
+]
+var tops = [
+  document.querySelector('.topmore a:nth-child(2)'),
+  document.querySelector('.topmore a:nth-child(3)'),
+  document.querySelector('.topmore a:nth-child(4)'),
+  document.querySelector('.topmore a:nth-child(5)'),
+]
+var he = document.querySelector('.topmore a:nth-child(6)');
+
+
+for (let i = 0; i < tops.length; i++) {
+  tops[i].top = conList[i].offsetTop;
+  tops[i].height = conList[i].offsetHeight;
+  // console.log(tops[i].top);
+  let that = this
+  tops[i].onclick = function () {
+    animate2(document.documentElement, {
+      scrollTop: this.top + 100
+    }, 500);
+    for (let j = 0; j < tops.length; j++) {
+      tops[j].style.backgroundColor = ''
+    }
+    tops[i].style.backgroundColor = tops[i].getAttribute('color')
   }
-  window.scrollTo(0,1380);
-  this.style.backgroundColor = '#ef7f7c'
+
 }
-tops[3].onclick = function () {
-  for (let i = 0; i < tops.length; i++) {
-    tops[i].style.backgroundColor = ''
-  }
-  window.scrollTo(0,2050);
-  this.style.backgroundColor = '#82ef69'
-}
-tops[4].onclick = function () {
-  for (let i = 0; i < tops.length; i++) {
-    tops[i].style.backgroundColor = ''
-  }
-  window.scrollTo(0,2900);
-  this.style.backgroundColor = '#244eeb'
-}
-tops[8].onclick = function () {
-  for (let i = 0; i < tops.length; i++) {
-    tops[i].style.backgroundColor = ''
-  }
-  window.scrollTo(0,3550);
-  this.style.backgroundColor = '#244eeb'
-}
-tops[9].onclick = function () {
-  for (let i = 0; i < tops.length; i++) {
-    tops[i].style.backgroundColor = ''
-  }
-  window.scrollTo(0,0);
-}
+//回顶部
+he.onclick = function () {
+  //窗口document.documentElement
+  animate2(document.documentElement, {
+    scrollTop: 0
+  }, 500)
+};
 
 
